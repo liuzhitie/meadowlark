ca1634b8df22671905634a79e44c369bca560c6a
liuzhitie <elite_liuzhitie@163.com>
2022-07-20 09:01:18 +0800

    Move 'fortune. cookie' into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,15 +1,9 @@
-const express = require('express')
-const expressHandlebars = require('express-handlebars')
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const fortune = require('./lib/fortune');
+const app = express();
 
-const app = express()
 
-const fortunes = [
-    'Conquer your fears or they will conquer you.',
-    'Rivers need springs.',
-    'Do not fear what you do not know.',
-    'You will have a pleasant surprise.',
-    'Whenever possible, keep it simple.'
-]
 
 // configure Handlebars view engine
 app.engine('handlebars', expressHandlebars.engine({
@@ -20,10 +14,9 @@ app.set('view engine', 'handlebars')
 const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
-app.get('/', (req, res) => res.render('home'))
+app.get('/', (req, res) => res.render('home'));
 app.get('/about', (req, res) => {
-  const randomFortune =  fortunes[Math.floor(Math.random() * fortunes.length)]
-  res.render('about', {fortune:randomFortune})
+  res.render('about', {fortune:fortune.getFortune()});
 })
 
 // custom 404 page
